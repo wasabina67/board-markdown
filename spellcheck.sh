c91684971ee1f58dd3fadcc66d24c6df51c00381
wasabina67 <wasabina67@gmail.com>
2025-07-05 16:17:52 +0900

    Update spellcheck.sh

--- spellcheck.sh
+++ spellcheck.sh
@@ -3,7 +3,15 @@
 cspell .
 echo "====="
 
-files=( $(find . -type f ! -path "./.git/*" ! -path "./node_modules/*" ! -name "*.svg" ! -name "package-lock.json") )
+files=(
+    $(find . \
+        -type f \
+        ! -path "./.git/*" \
+        ! -path "./node_modules/*" \
+        ! -name "*.svg" \
+        ! -name "package-lock.json"
+    )
+)
 printf '%s\n' "${files[@]}"
 echo "find: Files found: ${#files[@]}"
 echo "====="
