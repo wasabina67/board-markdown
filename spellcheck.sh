aae8f9581161d1812ff2de071d36fadea088bfb2
wasabina67 <wasabina67@gmail.com>
2025-07-05 16:15:57 +0900

    Update spellcheck.sh

--- spellcheck.sh
+++ spellcheck.sh
@@ -5,5 +5,5 @@ echo "====="
 
 files=( $(find . -type f ! -path "./.git/*" ! -path "./node_modules/*" ! -name "*.svg" ! -name "package-lock.json") )
 printf '%s\n' "${files[@]}"
-echo "Files found: ${#files[@]}"
+echo "find: Files found: ${#files[@]}"
 echo "====="
