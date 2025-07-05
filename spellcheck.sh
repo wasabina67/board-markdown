9a8ad690748436a625bb1b1602f0775e7e8034bc
wasabina67 <wasabina67@gmail.com>
2025-07-05 16:06:56 +0900

    Update spellcheck.sh

--- spellcheck.sh
+++ spellcheck.sh
@@ -3,3 +3,7 @@
 echo "====="
 cspell .
 echo "====="
+
+result=`find . -type f ! -path "./.git/*" ! -path "./node_modules/*" ! -name "*.svg" ! -name "package-lock.json"`
+echo "$result" | tr ' ' '\n'
+echo "====="
